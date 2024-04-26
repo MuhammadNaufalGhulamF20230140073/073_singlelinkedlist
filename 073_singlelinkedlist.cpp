1fd0f41c77fb44ca377e75af9aafb844a334c090
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-04-26 14:27:41 +0700

    membuat  fungsi bernama traverse() yang bertujuan untuk mencetak setiap node dalam linked list yang berisi data mahasiswa dengan nomor mahasiswa (nim) dan nama.

--- 073_singlelinkedlist.cpp
+++ 073_singlelinkedlist.cpp
@@ -89,6 +89,21 @@ bool listEmpty() {
         return false;
 }
 
+void traverse(){
+    if( listEmpty()){
+        cout << "list Kosong" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
+    else{
+        Node* currentNode = START;
+        while ( currentNode != NULL){
+            cout << "NIM: " << currentNode->noMhs << ", Nama:. " << currentNode->name << endl;
+            currentNode = currentNode->next;
+        }
+    }
+
 
 
 
